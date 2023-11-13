e7bdc9a1ca4f097a5eb75cd2d419e7f5e4c24a32
Chen Liu <chen.liu.cl2482@yale.edu>
2023-11-13 00:59:13 -0500

    minor aesthetic changes

--- src/barplot.py
+++ src/barplot.py
@@ -10,7 +10,7 @@ def sbplot(ax: plt.Axes,
            method_list: List[str],
            data_dict: Dict[str, np.array],
            show_individual_points: bool = True,
-           individal_point_size: float = 8,
+           individal_point_size: float = 10,
            labelsize: int = 16) -> plt.Axes:
     '''
     Statistical Bar Plot.
@@ -23,7 +23,7 @@ def sbplot(ax: plt.Axes,
 
     if show_individual_points:
         df = pd.DataFrame(data_arr.T, columns=method_list)
-        sns.swarmplot(ax=ax, data=df, size=individal_point_size, palette=colors, alpha=0.5)
+        sns.swarmplot(ax=ax, data=df, size=individal_point_size, color='k', marker="$\circ$")
 
     ax.spines[['right', 'top']].set_visible(False)
     ax.tick_params(axis='both', which='major', labelsize=labelsize)
@@ -32,11 +32,11 @@ def sbplot(ax: plt.Axes,
         ax.bar(method_list[i],
                np.mean(data_dict[method_list[i]]),
                yerr=np.std(data_dict[method_list[i]]),
-               color='none',
-               edgecolor=colors[i],
-               width=0.4,
-               linewidth=3,
-               capsize=8,
+               edgecolor=(0, 0, 0, 1),
+               facecolor=(*colors[i][:3], 0.5), # set facecolor-specific alpha.
+               width=0.5,
+               linewidth=1.5,
+               capsize=10,
                error_kw={'capthick': 3, 'elinewidth': 3, 'alpha': 1})
 
     return ax
@@ -46,19 +46,19 @@ def test_sbplot():
     method_list = ['GRU-D', 'GRU-dt', 'ODE-RNN']
 
     auroc_dict = {
-        'GRU-D': np.clip(np.random.normal(loc=0.92, scale=0.02, size=(10,)), 0, 1),
+        'GRU-D': np.clip(np.random.normal(loc=0.92, scale=0.04, size=(10,)), 0, 1),
         'GRU-dt': np.clip(np.random.normal(loc=0.75, scale=0.03, size=(10,)), 0, 1),
         'ODE-RNN': np.clip(np.random.normal(loc=0.98, scale=0.02, size=(10,)), 0, 1),
     }
 
     acc_dict = {
-        'GRU-D': np.clip(np.random.normal(loc=0.89, scale=0.02, size=(10,)), 0, 1),
-        'GRU-dt': np.clip(np.random.normal(loc=0.81, scale=0.03, size=(10,)), 0, 1),
-        'ODE-RNN': np.clip(np.random.normal(loc=0.94, scale=0.02, size=(10,)), 0, 1),
+        'GRU-D': np.clip(np.random.normal(loc=0.89, scale=0.05, size=(10,)), 0, 1),
+        'GRU-dt': np.clip(np.random.normal(loc=0.81, scale=0.04, size=(10,)), 0, 1),
+        'ODE-RNN': np.clip(np.random.normal(loc=0.94, scale=0.03, size=(10,)), 0, 1),
     }
 
     f1_dict = {
-        'GRU-D': np.clip(np.random.normal(loc=0.92, scale=0.02, size=(10,)), 0, 1),
+        'GRU-D': np.clip(np.random.normal(loc=0.92, scale=0.04, size=(10,)), 0, 1),
         'GRU-dt': np.clip(np.random.normal(loc=0.87, scale=0.03, size=(10,)), 0, 1),
         'ODE-RNN': np.clip(np.random.normal(loc=0.95, scale=0.02, size=(10,)), 0, 1),
     }
@@ -68,18 +68,18 @@ def test_sbplot():
     fig = plt.figure(figsize=(14, 6))
     ax = fig.add_subplot(1, 3, 1)
     ax = sbplot(ax, method_list, acc_dict)
-    ax.set_ylabel(r'AUROC $\uparrow$', fontsize=18)
-    ax.set_xlabel('Methods', fontsize=18)
+    ax.set_ylabel('AUROC', fontsize=18)
+    ax.set_ylim([0, 1.05])
 
     ax = fig.add_subplot(1, 3, 2)
     ax = sbplot(ax, method_list, acc_dict)
-    ax.set_ylabel(r'Accuracy $\uparrow$', fontsize=18)
-    ax.set_xlabel('Methods', fontsize=18)
+    ax.set_ylabel('Accuracy', fontsize=18)
+    ax.set_ylim([0, 1.05])
 
     ax = fig.add_subplot(1, 3, 3)
     ax = sbplot(ax, method_list, f1_dict)
-    ax.set_ylabel(r'F1 Score $\uparrow$', fontsize=18)
-    ax.set_xlabel('Methods', fontsize=18)
+    ax.set_ylabel('F1 Score', fontsize=18)
+    ax.set_ylim([0, 1.05])
 
     fig.tight_layout()
     fig.savefig('../assets/sbplot_example.png')
