b1c1ec20fa92edc78a86a6d360a720582590821c
Chen Liu <chen.liu.cl2482@yale.edu>
2023-11-13 19:03:28 -0500

    add functionality to plot asterisks for p-values

--- src/barplot.py
+++ src/barplot.py
@@ -9,18 +9,36 @@ from matplotlib import pyplot as plt
 def sbplot(ax: plt.Axes,
            method_list: List[str],
            data_dict: Dict[str, np.array],
+           pvals_dict: Dict[str, float] = None,
            show_individual_points: bool = True,
            individal_point_size: float = 10,
-           labelsize: int = 16) -> plt.Axes:
+           labelsize: int = 16,
+           y_buffer_ratio: int = 0.05,
+           ymin: float = None,
+           ymax: float = None) -> plt.Axes:
     '''
     Statistical Bar Plot.
     Blot the bars and display the significance levels.
+
+    Arguments
+    ---------
+    - `method_list`:
+        A list of method strings, each corresponding to a bar in the plot.
+    - `data_dict`:
+        A dict, with each key being a method string and the value being a 1D numpy array.
+    - `pvals_dict`:
+        A dict with the following format:
+        {'$method1 vs $method2': $p-value, ...}
+        The key shall use ' vs ' to connect two method strings,
+        both of which mentioned in `method_list`.
     '''
 
     n_bars = len(method_list)
     colors = sns.color_palette('muted', n_bars)
     data_arr = np.array([data_dict[k] for k in method_list])
 
+    # Plot the individual points.
+    # Seaborn works the best with dataframes, hence the conversion.
     if show_individual_points:
         df = pd.DataFrame(data_arr.T, columns=method_list)
         sns.swarmplot(ax=ax, data=df, size=individal_point_size, color='k', marker="$\circ$")
@@ -28,6 +46,7 @@ def sbplot(ax: plt.Axes,
     ax.spines[['right', 'top']].set_visible(False)
     ax.tick_params(axis='both', which='major', labelsize=labelsize)
 
+    # Plot the bars one by one.
     for i in range(n_bars):
         ax.bar(method_list[i],
                np.mean(data_dict[method_list[i]]),
@@ -39,47 +58,108 @@ def sbplot(ax: plt.Axes,
                capsize=10,
                error_kw={'capthick': 3, 'elinewidth': 3, 'alpha': 1})
 
+    # Compute the range of y for display.
+    ymax_baseline = np.max(data_arr) if ymax is None else ymax
+    ymin_baseline = np.min(data_arr) if ymin is None else ymin
+    height_range = ymax_baseline - ymin_baseline
+    y_buffer = y_buffer_ratio * height_range
+    if ymin is not None:
+        ymin = min(ymin, np.min(data_arr) - y_buffer)
+    else:
+        ymin = np.min(data_arr) - y_buffer
+
+    # Plot the indicators of p-values.
+    ymax_baseline += y_buffer
+
+    if pvals_dict is not None:
+        for method_pair in pvals_dict.keys():
+            method1, method2 = method_pair.split(' vs ')
+
+            # Each method string shall appear in `method_list` once and only once.
+            assert (np.array(method_list) == method1).sum() == 1
+            assert (np.array(method_list) == method2).sum() == 1
+            method1_idx = np.where(np.array(method_list) == method1)[0].item()
+            method2_idx = np.where(np.array(method_list) == method2)[0].item()
+
+            # Plot the horizontal lines, p-values, and asterisks.
+            pval = pvals_dict[method_pair]
+            asterisks = pval_to_asterisk(pval=pval)
+            line_x = [method1_idx, method2_idx]
+            line_y = [ymax_baseline, ymax_baseline]
+            ax.plot(line_x, line_y, 'k', linewidth=1)
+            ax.text(np.mean(line_x), ymax_baseline, asterisks, horizontalalignment='center')
+
+            ymax_baseline += y_buffer
+
+    ax.set_ylim([ymin, ymax_baseline])
+
     return ax
 
+def pval_to_asterisk(pval: float) -> str:
+    '''
+    Assign the proper number of asterisks to the given p-value.
+    '''
+    assert pval >= 0, '`pval_to_asterisk`: p-value has to be >= 0.'
+    if pval < 0.0001:
+        return '* * * *'
+    elif pval < 0.001:
+        return '* * *'
+    elif pval < 0.01:
+        return '* *'
+    elif pval < 0.05:
+        return '*'
+    else:
+        return 'ns'
 
 def test_sbplot():
+    np.random.seed(1)
+
     method_list = ['GRU-D', 'GRU-dt', 'ODE-RNN']
 
-    auroc_dict = {
+    auroc_data_dict = {
         'GRU-D': np.clip(np.random.normal(loc=0.92, scale=0.04, size=(10,)), 0, 1),
         'GRU-dt': np.clip(np.random.normal(loc=0.75, scale=0.03, size=(10,)), 0, 1),
         'ODE-RNN': np.clip(np.random.normal(loc=0.98, scale=0.02, size=(10,)), 0, 1),
     }
+    auroc_pvals_dict = {
+        'GRU-D vs ODE-RNN': 0.01,
+        'GRU-dt vs ODE-RNN': 5e-5,
+    }
 
-    acc_dict = {
+    acc_data_dict = {
         'GRU-D': np.clip(np.random.normal(loc=0.89, scale=0.05, size=(10,)), 0, 1),
         'GRU-dt': np.clip(np.random.normal(loc=0.81, scale=0.04, size=(10,)), 0, 1),
         'ODE-RNN': np.clip(np.random.normal(loc=0.94, scale=0.03, size=(10,)), 0, 1),
     }
+    acc_pvals_dict = {
+        'GRU-D vs ODE-RNN': 0.01,
+        'GRU-dt vs ODE-RNN': 0.001,
+    }
 
-    f1_dict = {
+    f1_data_dict = {
         'GRU-D': np.clip(np.random.normal(loc=0.92, scale=0.04, size=(10,)), 0, 1),
         'GRU-dt': np.clip(np.random.normal(loc=0.87, scale=0.03, size=(10,)), 0, 1),
         'ODE-RNN': np.clip(np.random.normal(loc=0.95, scale=0.02, size=(10,)), 0, 1),
+    }
+    f1_pvals_dict = {
+        'GRU-D vs ODE-RNN': 0.01,
+        'GRU-dt vs ODE-RNN': 3e-4,
     }
 
     plt.rcParams['font.family'] = 'serif'
     plt.rcParams['legend.fontsize'] = 12
     fig = plt.figure(figsize=(14, 6))
     ax = fig.add_subplot(1, 3, 1)
-    ax = sbplot(ax, method_list, acc_dict)
+    ax = sbplot(ax=ax, method_list=method_list, data_dict=auroc_data_dict, pvals_dict=auroc_pvals_dict, ymin=0)
     ax.set_ylabel('AUROC', fontsize=18)
-    ax.set_ylim([0, 1.05])
 
     ax = fig.add_subplot(1, 3, 2)
-    ax = sbplot(ax, method_list, acc_dict)
+    ax = sbplot(ax=ax, method_list=method_list, data_dict=acc_data_dict, pvals_dict=acc_pvals_dict, ymin=0)
     ax.set_ylabel('Accuracy', fontsize=18)
-    ax.set_ylim([0, 1.05])
 
     ax = fig.add_subplot(1, 3, 3)
-    ax = sbplot(ax, method_list, f1_dict)
+    ax = sbplot(ax=ax, method_list=method_list, data_dict=f1_data_dict, pvals_dict=f1_pvals_dict, ymin=0)
     ax.set_ylabel('F1 Score', fontsize=18)
-    ax.set_ylim([0, 1.05])
 
     fig.tight_layout()
     fig.savefig('../assets/sbplot_example.png')
